985dab203f2b2f8c51dfa4a1d184c6408a8ad44d
Evan Howard <evanhoward@college.harvard.edu>
2023-01-30 02:54:18 -0500

    completed todo app

---
+++ CommentedTodoApp/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  // #Component
+  import Todos from "./Todos.svelte";
+
+  let todos = [
+    { id: 1, name: "Create a Svelte starter app", completed: true },
+    { id: 2, name: "Create your first component", completed: true },
+    { id: 3, name: "Complete the rest of the tutorial", completed: false }
+  ];
+</script>
+
+<Todos {todos} />
